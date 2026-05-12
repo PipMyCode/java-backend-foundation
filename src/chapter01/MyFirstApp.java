e96fdbd62d695008f81e3ee09bcffbeae43586b3
PipMyCode <adigunmarcus@gmail.com>
2026-05-12 14:05:52 -0400

    Chapter 1: My first java class is live

---
+++ src/chapter01/MyFirstApp.java
@@ -0,0 +1,10 @@
+package chapter01;
+
+public class MyFirstApp {
+    public static void main(String[] args) {
+        System.out.println("I rule!");
+        System.out.println("The World");
+    }
+
+
+}
